d81e0cd45f7e4bfd420d4d0cd13f49784b2fd6e7
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2021-02-26 21:12:38 +0300

    [HACK] Implement spec tests runner for ir interpreter

--- compiler/tests-spec/build.gradle.kts
+++ compiler/tests-spec/build.gradle.kts
@@ -7,8 +7,8 @@ dependencies {
     testCompile(projectTests(":compiler"))
     testImplementation(projectTests(":compiler:test-infrastructure"))
     testImplementation(projectTests(":compiler:tests-common-new"))
-    testRuntimeOnly(platform("org.junit:junit-bom:5.7.0"))
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter")
+
+    testApiJUnit5()
 
     testCompileOnly(intellijDep()) {
         includeJars("groovy", "groovy-xml", rootProject = rootProject)
@@ -30,8 +30,9 @@ sourceSets {
 
 testsJar()
 
-projectTest(parallel = true) {
+projectTest(parallel = true, jUnit5Enabled = true) {
     workingDir = rootDir
+    useJUnitPlatform()
 }
 
 val generateSpecTests by generator("org.jetbrains.kotlin.spec.utils.tasks.GenerateSpecTestsKt")
